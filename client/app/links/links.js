fa945af8300942fae9eadae8e369e8722c2b88d5
SYU88 <sarah.m.yu88@gmail.com>
2015-06-12 19:17:38 -0700

    Links now sorted by visit

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -5,6 +5,9 @@ angular.module('shortly.links', [])
   $scope.data = {};
   $scope.getLinks = function(){
     Links.getLinks().then(function(links){
+      links.sort(function(a,b){
+        return b.visits-a.visits;
+      });
       $scope.data.links = links;
       console.log($scope.data);
     }).catch(function (error) {
