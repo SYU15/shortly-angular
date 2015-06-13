d7aa828e411caaebb5b59cecdb4804c16e2caaeb
SYU88 <sarah.m.yu88@gmail.com>
2015-06-12 17:31:19 -0700

    Starting to work on validation

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,12 +3,22 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link={};
-
+  $scope.bool=false;
+  var isValidUrl = function(url) {
+    return url.match(rValidUrl);
+  };
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
   $scope.addLink = function(){
-    Links.postLink($scope.link).then(function(){
+    if(isValidUrl($scope.link)){
+      Links.postLink($scope.link).then(function(){
+        $scope.link = '';
+      }).catch(function (error) {
+        console.error(error);
+      });
+    } else {
       $scope.link = '';
-    }).catch(function (error) {
-      console.error(error);
-    });
+      $scope.bool=true;
+
+    }
   }
 });
